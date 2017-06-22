3814278a14a77dbf31a20cc7848e75d00add45ba
fusheng1990 <fusheng1990@gmail.com>
2017-06-22 22:31:17 +0800

    Update main.cpp

--- single_number/main.cpp
+++ single_number/main.cpp
@@ -4,11 +4,11 @@ int main()
 {
   std::cin<<A<<std::endl;
   int k = A[0];
-  int n = sizeof(A)/sizeof(A[0]); //cal
+  int n = sizeof(A)/sizeof(A[0]); //calculate the size of an array
 	std::cout<<n<<std::endl;
 	for (int i =1;i<n;i++)
 	{
-		k = k^A[i];
+		k = k^A[i];  //异或  出现过的都会消失
 	}
 	std::cout<<k<<std::endl;
 
