0d51320cd7859e076fcf32fc0deb666bb3950623
fusheng1990 <fusheng1990@gmail.com>
2017-06-22 10:29:41 -0400

    Update main.cpp

--- single_number/main.cpp
+++ single_number/main.cpp
@@ -4,5 +4,13 @@ int main()
 {
   std::cin<<A<<std::endl;
   int k = A[0];
-  
+  int n = sizeof(A)/sizeof(A[0]); //cal
+	std::cout<<n<<std::endl;
+	for (int i =1;i<n;i++)
+	{
+		k = k^A[i];
+	}
+	std::cout<<k<<std::endl;
+
+}
 }
